d00ad35f97e2e33bf4ec0dd4a311485adae89d4c
Jack Tregurtha <jacktregurtha@gmail.com>
2017-11-13 17:43:13 -0500

    Initial stepper motor code

--- src/WireBender.ino
+++ src/WireBender.ino
@@ -1,7 +1,27 @@
+/*
+* Wire Bender
+* Description:
+* Author:
+* Date: 12/2/17
+*/
+
 // create servo object to control a servo
 Servo myservo;
 // Pin to control the servo
 const int servo_pin = D0;
+// Step pin for stepper motor
+const int step_pin = D1;
+// Direction pin for stepper motor
+const int direction_pin = D2;
+// Steps for 360 degree stepper turn
+const int STEPS_PER_TURN = 200;
+// Minimum delay between steps in microseconds
+const int STEP_DELAY = 5000;
+// The angle of a step for the stepper motor
+const float STEP_ANGLE = 1.8;
+
+// Current stepper angle
+float angle = 0.0;
 // variable to read the value from the analog pin
 int val;
 
@@ -14,10 +34,14 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     // Bends the wire apprx. 50 degrees
-    myservo.write(100);
-    delay(2000);
-    myservo.write(165);
-    delay(2000);
+    // myservo.write(100);
+    // delay(2000);
+    // myservo.write(165);
+    // delay(2000);
+    int n = Serial.parseInt();
+    steps(n);
+
+
 
     // NOTE: Used to parse integer inputs and write them to servo
     // val = Serial.parseInt();
@@ -38,5 +62,40 @@ void loop() {
     //   Serial.println(" is outside of valid Servo range");
     // }
   }
+}
 
+// Steps the stepper motor number_of_steps steps. If number_of_steps < 0 the
+// motor will rotate in the opposite direction.
+void steps(int number_of_steps) {
+  bool move_forward = true;
+  // Establishing the direction
+  if (number_of_steps < 0) {
+    move_forward = false;
+    number_of_steps = -number_of_steps;
+  }
+  // Generating the steps
+  for (int i = 0; i < number_of_steps; i++) {
+    step(move_forward);
+    if (move_forward) {
+      angle = (angle + STEP_ANGLE) % 360;
+    } else {
+      angle = (angle - STEP_ANGLE) % 360;
+    }
+    // Delay for proper speed
+    delayMicroseconds(STEP_DELAY);
+  }
+}
+
+// Steps the stepper motor a single step either forward or backward
+void step(bool forward) {
+  // setting the direction
+  if (forward == true) {
+    digitalWrite(direction_pin, HIGH);
+  } else {
+    digitalWrite(direction_pin, LOW);
+  }
+  // creating a step
+  digitalWrite(step_pin, HIGH);
+  // minimum delay is 1.9us
+  digitalWrite(step_pin, LOW);
 }
