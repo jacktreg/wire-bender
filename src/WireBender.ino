a0d0286dbfd4e475740da62f8127e7ccd6d295ab
Adler Faulkner <aif28@cornell.edu>
2017-11-14 18:41:51 -0500

    created webform and code to use solenoid

--- src/WireBender.ino
+++ src/WireBender.ino
@@ -1,42 +1,38 @@
 // create servo object to control a servo
-Servo myservo;
+Servo bend_servo;
+// Cloud functions must return int and take one Stringint
+int bendFunction(String command);
 // Pin to control the servo
 const int servo_pin = D0;
+const int solenoid_pin = D1;
 // variable to read the value from the analog pin
 int val;
 
 void setup() {
+  pinMode(solenoid_pin, OUTPUT);
   // attaches the servo on pin 9 to the servo object
-  myservo.attach(servo_pin);
-  Serial.begin(9600);
+  bend_servo.attach(servo_pin);
+  // Initialize the servo to a certain angle
+  bend_servo.write(97);
+  // Register the cloud function with a name and with the function
+  // (Name of function, function call)
+  Particle.function("bend", bendFunction);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    // Bends the wire apprx. 50 degrees
-    myservo.write(100);
-    delay(2000);
-    myservo.write(165);
-    delay(2000);
 
-    // NOTE: Used to parse integer inputs and write them to servo
-    // val = Serial.parseInt();
-    // Serial.print("recieving: ");
-    // Serial.println(val);
+}
 
-    // scale it to use it with the servo (value between 0 and 180)
-    // if (val < 180 && val > 0){
-    //   // sets the servo position according to the scaled value
-    //   myservo.write(val);
-    //   Serial.print("Servo moving to: ");
-    //   Serial.println(myservo.read());
-    //   // waits for the servo to get there
-    //   delay(2000);
-    // } else {
-    //   Serial.print("Warning: value ");
-    //   Serial.print(val);
-    //   Serial.println(" is outside of valid Servo range");
-    // }
-  }
+// this function automatically gets called upon a matching POST request
+int bendFunction(String command) {
+  int angle = command.toInt();
 
+  digitalWrite(solenoid_pin, HIGH);
+  delay(500);
+  bend_servo.write(angle);
+  delay(1000);
+  bend_servo.write(97);
+  digitalWrite(solenoid_pin, LOW);
+
+  return 1;
 }
