aac8290a4f8b0c3d1cb5a72389cb56464e29ffa8
rgw66 <rgw66@cornell.edu>
2017-11-27 18:06:37 -0500

    file processing

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function readTextFile()
+function readTextFile(filename)
 {
     var rawFile = new XMLHttpRequest();
     var data = [];
@@ -17,16 +17,44 @@ function readTextFile()
                     data.push(actions[0] + "-" + actions[1]);
                   }
                 });
-                $.("#form_data").val(data.join(",")+",");
-                $.("#post_form").submit();
+                console.log(data.join(",") + ",")
+                // $.("#form_data").val(JSON.stringify(data));
+                // $.("#post_form").submit();
             }
         }
     }
     rawFile.send(null);
 }
-$( document ).ready(function() {
-  $('#file_form').on('submit', function () {
-      readTextFile();
-      return false;
+
+function processContent(content){
+  var data = [];
+  var allLines = content.split(/\r\n|\n/);
+  $.each(allLines, function(index, line){
+    var actions = line.split(" ");
+    if (actions.length >= 2) {
+      data.push(actions[0] + actions[1]);
+    }
   });
+  console.log(data.join(",") + ",")
+}
+
+function processFile(event) {
+	const input = event.target
+  if ('files' in input && input.files.length > 0) {
+	  var file = input.files[0];
+    const reader = new FileReader();
+    var result = new Promise((resolve, reject) => {
+      reader.onload = event => resolve(event.target.result)
+      reader.onerror = error => reject(error)
+      reader.readAsText(file)
+    })
+    result.then(content => {
+    	processContent(content)
+    }).catch(error => console.log(error))
+  }
+}
+
+$( document ).ready(function() {
+  document.getElementById('file')
+    .addEventListener('change', processFile)
 });
