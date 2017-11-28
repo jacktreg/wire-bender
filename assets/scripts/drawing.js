028de5464455e4f79d0248411abc8951336409bd
Jack Tregurtha <jacktregurtha@gmail.com>
2017-11-28 18:36:24 -0500

    Drawing interface improvements, and initial drawing path rectification.

--- assets/scripts/drawing.js
+++ assets/scripts/drawing.js
@@ -2,18 +2,17 @@
 var path;
 
 function onMouseDown(event) {
-	// If we produced a path before, deselect it:
-	if (path) {
-		path.selected = false;
+  // If we haven't created the path yet, initialize
+	if (!path) {
+		// Create a new path and set its stroke color to black:
+		path = new Path({
+			segments: [event.point],
+			strokeColor: 'black',
+			// Select the path, so we can see its segment points:
+			fullySelected: true
+		});
 	}
 
-	// Create a new path and set its stroke color to black:
-	path = new Path({
-		segments: [event.point],
-		strokeColor: 'black',
-		// Select the path, so we can see its segment points:
-		fullySelected: true
-	});
 }
 
 // While the user drags the mouse, points are added to the path
@@ -27,7 +26,9 @@ function onMouseUp(event) {
 	var segmentCount = path.segments.length;
 
 	// When the mouse is released, simplify it:
-	path.simplify(10);
+	path.smooth();
+	path.simplify(25);
+	path.flatten(25);
 
 	// Select the path, so we can see its segments:
 	path.fullySelected = true;
@@ -35,5 +36,4 @@ function onMouseUp(event) {
 	var newSegmentCount = path.segments.length;
 	var difference = segmentCount - newSegmentCount;
 	var percentage = 100 - Math.round(newSegmentCount / segmentCount * 100);
-	path = null;
 }
